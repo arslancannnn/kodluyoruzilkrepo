b96ab64b37c390e8ed35bf94471661ccc342831b
arslancannnn <canarslan14@icloud.com>
2022-08-21 20:16:11 +0300

    Daire alan eklendi

---
+++ www.patica.dev/Java/TemelKavramlar/DaireAlan.java
@@ -0,0 +1,18 @@
+package TemelKavramlar;
+import java.util.Scanner;;
+
+public class DaireAlan {
+    public static void main(String[] args) {
+        double P=3.14, A,R,Alan;
+        Scanner Input = new Scanner(System.in);
+
+        System.out.print("Yarıçapı girin:");
+        R=Input.nextDouble();
+        System.out.print("Açı çlçüsünü girin");
+        A=Input.nextDouble();
+
+        Alan=(P*R*R*A)/360;
+        System.out.println("Alan: "+Alan);
+    }
+    
+}
