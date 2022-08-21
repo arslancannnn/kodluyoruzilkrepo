3cfe98f5095501b921151b291ade7ee00e51c246
arslancannnn <canarslan14@icloud.com>
2022-08-21 14:45:01 +0300

    Java ödev eklendi

---
+++ www.patica.dev/Java/Temel Kavramlar/OrtalamaHesaplama.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;;;
+
+public class OrtalamaHesaplama{
+    public static void main(String[] args) {
+    
+    Scanner Input = new Scanner(System.in);
+
+        System.out.println("Matematik notunu girin;");
+        int Mat = Input.nextInt();
+
+        System.out.println("Fizik notunu girin;");
+        int Fizik = Input.nextInt();
+
+        System.out.println("Kimya notunu girin;");
+        int Kimya = Input.nextInt();
+
+        System.out.println("Türkçe notunu girin;");
+        int Tr = Input.nextInt();
+
+        System.out.println("Tarih notunu girin;");
+        int Tarih = Input.nextInt();
+
+        System.out.println("Müzik notunu girin;");
+        int Muzik = Input.nextInt();
+        
+        int Avg = (Mat+Fizik+Kimya+Tr+Tarih+Muzik)/6;
+
+    System.out.println("Ortalama: "+Avg);
+    System.out.println(Avg > 60 ? "Sınıfı geçti": "Sınıfı geçemedi");
+
+       
+    }
+}
