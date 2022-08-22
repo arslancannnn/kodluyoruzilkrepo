01cd6dc16c14445de4dcdf4620ec53965f1ff75a
arslancannnn <canarslan14@icloud.com>
2022-08-22 15:23:03 +0300

    Manav.java eklendi

---
+++ www.patica.dev/Java/TemelKavramlar/Manav.java
@@ -0,0 +1,32 @@
+package TemelKavramlar;
+import java.util.Scanner;;
+
+public class Manav {
+    public static void main(String[] args) {
+        double Armut=2.14, Elma=3.67, Domates=1.11,Muz=0.95,Patlican=5,Kilo,Tutar=0;
+        Scanner Input =new Scanner(System.in);
+        System.out.println("Armut kaç kilo:");
+        Kilo=Input.nextDouble();
+        Tutar += Armut*Kilo;
+
+        System.out.println("Elma kaç kilo:");
+        Kilo=Input.nextDouble();
+        Tutar += Elma*Kilo;
+
+        System.out.println("Domates kaç kilo:");
+        Kilo=Input.nextDouble();
+        Tutar += Domates*Kilo;
+
+        System.out.println("Muz kaç kilo:");
+        Kilo=Input.nextDouble();
+        Tutar += Muz*Kilo;
+
+        System.out.println("Patlıcan kaç kilo:");
+        Kilo=Input.nextDouble();
+        Tutar += Patlican*Kilo;
+
+        System.out.println("Toplam tutar: "+Tutar+" TL");
+        
+    }
+    
+}
