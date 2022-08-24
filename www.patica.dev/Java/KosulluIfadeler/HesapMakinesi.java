f6df971687686d73866bdf19bb4b669f4afdbfac
arslancannnn <canarslan14@icloud.com>
2022-08-24 13:11:21 +0300

    HesapMakinesi.java eklendi

---
+++ www.patica.dev/Java/KosulluIfadeler/HesapMakinesi.java
@@ -0,0 +1,37 @@
+package KosulluIfadeler;
+
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        int n1,n2,Select;
+
+        System.out.println("İlk sayıyı girin: ");
+        n1 = Input.nextInt();
+        System.out.println("İkinci sayıyı girin: ");
+        n2 = Input.nextInt();
+        System.out.println("Yapılacak işlem:");
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        Select=Input.nextInt();
+
+        switch(Select){
+            case 1:
+                System.out.println("Toplam :" +(n1+n2));
+                break;
+
+            case 2:
+                System.out.println("Fark :" +(n1-n2));
+                break;
+            case 3:
+                System.out.println("Çarpım :" +(n1*n2));
+                break;
+            case 4:
+                System.out.println("Bölüm :" +(n1/n2));
+                break;
+            
+        }
+        
+    }
+    
+}
