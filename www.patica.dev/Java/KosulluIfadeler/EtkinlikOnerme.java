c7f6f33ef5d77bf6250d3a0791e55100b2abe419
arslancannnn <canarslan14@icloud.com>
2022-08-26 16:08:55 +0300

    EtkinlikOnerme.java eklendi

---
+++ www.patica.dev/Java/KosulluIfadeler/EtkinlikOnerme.java
@@ -0,0 +1,24 @@
+package KosulluIfadeler;
+import java.util.Scanner;;
+
+public class EtkinlikOnerme {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int heat;
+
+        System.out.println("Sıcaklığı girin: ");
+        heat=inp.nextInt();
+
+        if(heat<5){
+            System.out.println("Bugün kayağa gidebilirsin");
+        }else if(heat>=5 && heat<=25){
+            if(heat<15)
+                System.out.println("Bugün sinemaya gidebilirsin");
+            if(heat>10)
+                System.out.println("Bugün pikniğe gidebilirsin");
+        }else 
+            System.out.println("Bugün yüzmeye gidebilirsin");
+    }
+    
+}
