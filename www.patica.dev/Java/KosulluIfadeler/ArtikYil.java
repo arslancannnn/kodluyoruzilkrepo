c7ebc4da31267a31f38c3a30d0ff466df6da108e
arslancannnn <canarslan14@icloud.com>
2022-08-26 18:34:08 +0300

    ArtikYil.java eklendi

---
+++ www.patica.dev/Java/KosulluIfadeler/ArtikYil.java
@@ -0,0 +1,19 @@
+package KosulluIfadeler;
+import java.util.Scanner;;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner inp =new Scanner(System.in);
+        System.out.println("Bir yıl girin: ");
+        int yil = inp.nextInt();
+
+        if(yil%4==0){
+            if(yil%100==0)
+                System.out.println(yil%400==0 ? yil+" bir artık yıldır." : yil+" bir artık yıl değildir");
+            else  
+                System.out.println(yil+" bir artık yıldır");   
+        }else
+            System.out.println(yil+" bir artık yıl değildir");
+    }
+    
+}
