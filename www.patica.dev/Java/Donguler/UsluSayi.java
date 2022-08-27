d67f67f72c6451f29bf72c9c5bb47320a3eaf68b
arslancannnn <canarslan14@icloud.com>
2022-08-27 15:49:51 +0300

    UsluSayi.java eklendi

---
+++ www.patica.dev/Java/Donguler/UsluSayi.java
@@ -0,0 +1,21 @@
+package Donguler;
+import java.util.Scanner;;
+
+public class UsluSayi {
+    public static void main(String[] args) {
+        Scanner inp= new Scanner(System.in);
+        int taban,us;
+        long sonuc=1;
+
+        System.out.print("Üssü alınacak sayı: ");
+        taban=inp.nextInt();
+
+        System.out.print("\nÜs olacak sayı: ");
+        us=inp.nextInt();
+
+        for (int i = 1; i <= us; i++) {
+            sonuc *= taban;
+        }
+        System.out.println("\n"+taban+"^"+us+" = "+sonuc);
+    }
+}
