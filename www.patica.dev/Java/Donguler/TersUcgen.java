fe0a1d5bf9e2ea2c4451bb128099add2095828a5
arslancannnn <canarslan14@icloud.com>
2022-08-28 14:48:38 +0300

    TersUcgen.java eklendi

---
+++ www.patica.dev/Java/Donguler/TersUcgen.java
@@ -0,0 +1,23 @@
+package Donguler;
+import java.util.Scanner;;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        try (Scanner inp = new Scanner(System.in)) {  
+            int num;
+          
+            System.out.println("Bir sayı girin: ");
+            num=inp.nextInt();
+
+            for (int i = num-1; i >= 0; i--) {
+                for (int j = 0; j < (num-i); j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= (2 * i + 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+        }
+    }
+}
