adc733c974efdbe11685aa5bad6577d5edc27882
arslancannnn <canarslan14@icloud.com>
2022-08-28 17:53:20 +0300

    asal.java eklendi

---
+++ www.patica.dev/Java/Donguler/Asal.java
@@ -0,0 +1,25 @@
+package Donguler;
+
+public class Asal {
+    public static void main(String[] args) {
+        for (int i = 2; i < 101; i++) {
+            if(i% 2 == 0 && i != 2){
+                continue;
+            }else if(i % 3 == 0 && i !=3){
+                continue;
+            }else if(i % 4 == 0){
+                continue;
+            }else if(i % 5 == 0 && i!=5){
+                continue;
+            }else if(i % 7 == 0 && i!=7){
+                continue;
+            }else if(i % 11 == 0 && i!=7){
+                continue;
+            }else{
+                System.out.print(i+" ");
+            }
+        
+        }
+    }
+    
+}
