4f08692b687300d1f7db213c838ceef1cd238e10
arslancannnn <canarslan14@icloud.com>
2022-08-28 18:12:20 +0300

    Fibonacci.java eklendi

---
+++ www.patica.dev/Java/Donguler/Fibonacci.java
@@ -0,0 +1,20 @@
+package Donguler;
+import java.util.Scanner;;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int num;
+        int bir=0,iki=1,d;
+
+        System.out.println("Bir sayı girin: ");
+        num=inp.nextInt();
+        System.out.print(bir+" "+iki+" ");
+        for (int i = 1; i <= num; i++) {
+            d=bir+iki;
+            System.out.print(d+" ");
+            bir=iki;
+            iki=d;
+        }
+    }
+}
