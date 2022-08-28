c672065b347614efde6dbc14f2f79d8539a79857
arslancannnn <canarslan14@icloud.com>
2022-08-28 18:58:04 +0300

    Asal.java hata giderildi

--- www.patica.dev/Java/Donguler/Asal.java
+++ www.patica.dev/Java/Donguler/Asal.java
@@ -7,13 +7,9 @@ public class Asal {
                 continue;
             }else if(i % 3 == 0 && i !=3){
                 continue;
-            }else if(i % 4 == 0){
-                continue;
             }else if(i % 5 == 0 && i!=5){
                 continue;
             }else if(i % 7 == 0 && i!=7){
-                continue;
-            }else if(i % 11 == 0 && i!=7){
                 continue;
             }else{
                 System.out.print(i+" ");
