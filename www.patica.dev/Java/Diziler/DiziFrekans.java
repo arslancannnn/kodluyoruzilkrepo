582d268f4823b97370d16a71f2d0089c0819c822
arslancannnn <canarslan14@icloud.com>
2022-09-05 21:33:11 +0300

    DiziFrekans.java eklendi

---
+++ www.patica.dev/Java/Diziler/DiziFrekans.java
@@ -0,0 +1,38 @@
+package Diziler;
+
+public class DiziFrekans {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 20, 20, 10, 10, 20, 5, 20};
+        int[] frekans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            frekans[i] = 1;
+        }  
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ((i != j) && (arr[i] == arr[j])) {
+                    frekans[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if ((i != j) && (arr[i] == arr[j])) {
+                    arr[j] = 0;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(frekans[i]>=1){
+                if(arr[i]!=0){
+                    System.out.println(arr[i]+" sayısı "+frekans[i]+" kere tekrar edildi");
+                }
+            }
+        }
+    }    
+    
+}
