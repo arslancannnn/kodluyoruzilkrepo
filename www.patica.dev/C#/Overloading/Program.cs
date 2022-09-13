fa6891e4db096ec5fc0b26ef787fe8b7279459cf
arslancannnn <canarslan14@icloud.com>
2022-09-14 00:09:36 +0300

    Overloading eklendi

---
+++ www.patica.dev/C#/Overloading/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // out parametreler
+        string sayi = "999";
+        bool sonuc = int.TryParse(sayi, out int outsayi);
+
+        if(sonuc)
+        {
+            Console.WriteLine("Başarılı");
+            Console.WriteLine(outsayi);
+        }else
+        {
+            Console.WriteLine("Başarısız");
+        }
+        Metotlar instance = new Metotlar();
+        instance.topla(4,5, out int toplamsonucu);
+        Console.WriteLine(toplamsonucu);
+
+        // Metot aşırı yükleme = overloading
+        int ifade = 999;
+        instance.ekranaYazdir(Convert.ToString(ifade));
+        instance.ekranaYazdir(ifade);
+        instance.ekranaYazdir("Eren","Bakıcı");
+
+
+    }
+}
+class Metotlar
+{
+    public void topla(int a, int b, out int toplam)
+    {
+        toplam = a+b;
+    }
+    public void ekranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void ekranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void ekranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1+veri2);
+    }
+}
