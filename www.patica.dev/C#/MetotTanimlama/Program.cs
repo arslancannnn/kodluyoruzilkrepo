26268a1e643b1d4332345fdc3791ade5d465ab4e
arslancannnn <canarslan14@icloud.com>
2022-09-13 23:42:27 +0300

    MetotTanimlama eklendi

---
+++ www.patica.dev/C#/MetotTanimlama/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        
+        Console.WriteLine(a+b);
+
+        int sonuc = topla(a,b);
+        Console.WriteLine(sonuc);
+
+        Metodlar ornek = new Metodlar();
+        ornek.ekranaYazdir(Convert.ToString(sonuc));
+
+        int sonuc2 = ornek.arttirVeTopla(ref a,ref b);
+        ornek.ekranaYazdir(Convert.ToString(sonuc2));
+        ornek.ekranaYazdir(Convert.ToString(a+b));
+
+    }
+    static int topla(int a, int b)
+    {
+        return a+b;
+    }
+}
+class Metodlar
+{
+    public void ekranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public int arttirVeTopla(ref int deger1,ref int deger2)
+    {
+        deger1 += 1;
+        deger2 += 1;
+        return deger1+deger2;
+    }
+}
